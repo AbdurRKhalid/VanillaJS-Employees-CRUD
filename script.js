9e4a28199c3ef99f4ce91ee553b10659e1ac838a
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2021-06-19 18:59:51 +0200

    Added Method for New Employee in Table.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function onFormSubmit() {
     var formData = readFormData();
+    insertNewRecord(formData);
 }
 
 function readFormData() {
@@ -12,4 +13,19 @@ function readFormData() {
 
     return formData;
     
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    var cell = newRow.insertCell(0);
+    cell.innerHTML = data.fullName;
+    var cell1 = newRow.insertCell(1);
+    cell1.innerHTML = data.designation;
+    var cell2 = newRow.insertCell(2);
+    cell2.innerHTML = data.salaray;
+    var cell3 = newRow.insertCell(3);
+    cell3.innerHTML = data.location;
+    var cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a>Edit</a> <a>Delete</a>`;
 }
